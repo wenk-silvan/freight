c4570fa0f43b946ab229ac6042b03abd48daa2bb
Silvan Wenk <silvan.wenk@gmail.com>
2019-06-26 11:53:12 +0200

    Thrust Rocket with Rigidbody and reduce mass

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,17 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody rigidBody;
+
+    private void Start()
     {
-           
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         this.ProcessInput();
     }
@@ -20,7 +19,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Thrusting");
+            this.rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
