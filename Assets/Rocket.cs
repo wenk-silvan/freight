37b18fee68739e43082b7ce68e0363a2be35f0a4
Silvan Wenk <silvan.wenk@gmail.com>
2019-08-11 17:54:24 +0200

    Add sound for completion of level and deadly collision

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,12 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField] private float rcsThrust = 100f;
     [SerializeField] private float mainThrust = 100f;
+    [SerializeField] private AudioClip mainEngine;
+    [SerializeField] private AudioClip explosion;
+    [SerializeField] private AudioClip victory;
 
     private Rigidbody rigidBody;
-    private AudioSource thrustingSound;
+    private AudioSource audioSource;
 
     private enum State
     {
@@ -16,12 +19,12 @@ public class Rocket : MonoBehaviour
         Transcending,
     }
 
-    [SerializeField] private State state = State.Alive;
+    private State state = State.Alive;
 
     private void Start()
     {
         this.rigidBody = GetComponent<Rigidbody>();
-        this.thrustingSound = GetComponent<AudioSource>();
+        this.audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,12 +32,11 @@ public class Rocket : MonoBehaviour
     {
         if (this.state == State.Dying || this.state == State.Transcending)
         {
-            this.thrustingSound.Stop();
             return;
         }
 
-        this.Thrust();
-        this.Rotate();
+        this.RespondToThrustInput();
+        this.RespondToRotateInput();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -44,23 +46,35 @@ public class Rocket : MonoBehaviour
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
                 break;
 
             case "Finish":
-                state = State.Transcending;
-                Invoke("LoadNextLevel", 1f);
+                OnFinishLevel();
                 break;
 
             case "Fuel":
-                print("Refilled fuel");
                 break;
 
             default:
-                state = State.Dying;
-                Invoke("LoadFirstLevel", 2f);
+                OnDeadlyCollision();
                 break;
         }
+    }
+
+    private void OnDeadlyCollision()
+    {
+        state = State.Dying;
+        this.audioSource.Stop();
+        this.audioSource.PlayOneShot(this.explosion);
+        Invoke("LoadFirstLevel", 2f);
+    }
+
+    private void OnFinishLevel()
+    {
+        state = State.Transcending;
+        this.audioSource.Stop();
+        this.audioSource.PlayOneShot(this.victory);
+        Invoke("LoadNextLevel", 1f);
     }
 
     private void LoadFirstLevel()
@@ -73,25 +87,39 @@ public class Rocket : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         var thrustThisFrame = this.mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
-            this.rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
-            if (!this.thrustingSound.isPlaying) this.thrustingSound.Play();
+            ApplyThrust(thrustThisFrame);
         }
         else
         {
-            this.thrustingSound.Stop();
+            this.audioSource.Stop();
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust(float thrustThisFrame)
+    {
+        this.rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+        if (!this.audioSource.isPlaying)
+        {
+            this.audioSource.PlayOneShot(this.mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
         var rotationThisFrame = this.rcsThrust * Time.deltaTime;
+        ApplyRotation(rotationThisFrame);
 
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * rotationThisFrame);
@@ -100,7 +128,5 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-
-        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
